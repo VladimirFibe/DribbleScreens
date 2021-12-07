42c88fd0ac383bb7e6ccfed11af56d0bf3c80030
Vladimir Fibe <motiw@me.com>
2021-12-07 21:49:30 +0600

    Initial Commit

---
+++ DribbleScreens/DribbleScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbleScreensApp.swift
+//  DribbleScreens
+//
+//  Created by Vladimir on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DribbleScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
